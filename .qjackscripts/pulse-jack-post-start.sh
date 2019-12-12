1d0be3b069a07adbf48985ab56f80a45031ed90f
ArchivalLegion <55867818+ArchivalLegion@users.noreply.github.com>
2019-12-12 16:03:01 -0500

    Update pulse-jack-post-start.sh

--- .qjackscripts/pulse-jack-post-start.sh
+++ .qjackscripts/pulse-jack-post-start.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/sh
 pactl load-module module-jack-sink sink_name="Vanilla-Jack" client_name="Standard-Sink" channels=2
 pactl load-module module-jack-sink sink_name="Unholy" client_name="Pulse-Soundboard" connect=0 channels=2
 pactl load-module module-jack-source channels=2
