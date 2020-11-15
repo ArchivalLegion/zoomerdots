7ea59b687209ca67cea6327ece87f110f3c702fa
ArchivalLegion <55867818+ArchivalLegion@users.noreply.github.com>
2020-11-15 11:17:23 -0500

    Lower sleep value

--- .qjackscripts/pulse-jack-pre-stop.sh
+++ .qjackscripts/pulse-jack-pre-stop.sh
@@ -3,4 +3,4 @@ SINKID=$(LANG=C pactl list | grep -B 1 "Name: module-jack-sink" | grep Module | 
 SOURCEID=$(LANG=C pactl list | grep -B 1 "Name: module-jack-source" | grep Module | sed 's/[^0-9]//g')
 pactl unload-module $SINKID
 pactl unload-module $SOURCEID
-sleep 5
+sleep 2
